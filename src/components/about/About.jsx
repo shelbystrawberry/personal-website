846b8b7849e730651d59c7dc4b94d26ed6778941
shelbystrawberry <shelbsgt500@gmail.com>
2022-05-10 03:07:38 -0400

    about page stage one: desc and image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,21 @@ import "./about.scss"
 export default function About() {
   return (
     <div className='about' id='about'>
-       about page
+      <div className="about-banner">
+        /about
+      </div>
+      <div className="about-body">
+        <div className="about-left">
+        I am currently a software engineering student with a 
+        focus in web development and some cloud infrastructure. 
+        Last year, I acquired my <em>"AWS Certified Developer" </em> 
+        certification. I continue to learn everyday whilst working
+        a part time job at a pediatrics company.
+        </div>
+        <div className="about-right">
+          <img src="assets/me.jpg" alt="" className="me-jpg"></img>
+        </div>
+      </div>
     </div>
   )
 }
