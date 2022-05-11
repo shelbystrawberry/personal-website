818e635666411919a935f7de909e98634d03dc04
shelbystrawberry <shelbsgt500@gmail.com>
2022-05-11 14:42:06 -0400

    added list of skills and descriptions

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,15 @@
 import "./about.scss"
 
 export default function About() {
+  const skill_stack = [
+    "Javascript ES6+",
+    "React.js",
+    "HTML & CSS",
+    "Python",
+    "Java", 
+    "AWS"
+  ];
+
   return (
     <div className='about' id='about'>
       <div className="about-banner">
@@ -8,11 +17,29 @@ export default function About() {
       </div>
       <div className="about-body">
         <div className="about-left">
-        I am currently a software engineering student with a 
-        focus in web development and some cloud infrastructure. 
-        Last year, I acquired my <em>"AWS Certified Developer" </em> 
-        certification. I continue to learn everyday whilst working
-        a part time job at a pediatrics company.
+          <div className="about-description">
+          I am currently a software engineering student with a 
+          focus in web development and some cloud infrastructure. 
+          Last year, I acquired my <em>"AWS Certified Developer" </em> 
+          certification. I continue to learn everyday whilst working
+          a part time job at a pediatrics company.
+          </div>
+          <div className="about-skills">
+            Here are some technologies I have been working with:
+            <ul className="skill-list">
+              {skill_stack.map(function (skill_item, i) {
+                return ( 
+                  <li>{skill_item}</li> 
+                );
+              })}
+            </ul>
+          </div>
+          <div className="about-extra">
+            Outside of school and work, I approach side projects like 
+            this portfolio and I follow the developments of science. 
+            I sometimes play video games and drive around in my car in 
+            the weekends. 
+          </div>
         </div>
         <div className="about-right">
           <img src="assets/me.jpg" alt="" className="me-jpg"></img>
