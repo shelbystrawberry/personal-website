c0789b54c7591c03550f1c25a45a52769cfe63df
shelbystrawberry <shelbsgt500@gmail.com>
2022-05-11 16:49:09 -0400

    containers for creations

--- src/components/creations/Creations.jsx
+++ src/components/creations/Creations.jsx
@@ -1,9 +1,18 @@
 import "./creations.scss"
+import { useEffect, useState } from "react"
 
 export default function Portfolio() {
   return (
     <div className="creations" id='creations'>
-        creations page
+        <div className="banner">
+          /portfolio
+        </div>
+        <div className="container">
+          <div className="item">
+            <img src="https://www.tompetty.com/sites/g/files/g2000007521/f/Sample-image10-highres.jpg" alt=""></img>
+            <h3>react-portfolio</h3>
+          </div>
+        </div>
     </div>
   )
 }
