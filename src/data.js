6940bbff67de60a1847282054df4cbad7ce0b90b
shelbystrawberry <shelbsgt500@gmail.com>
2022-05-13 15:04:48 -0400

    updated data.js descriptions

--- src/data.js
+++ src/data.js
@@ -11,13 +11,13 @@ export const creations = [
       title: "Resume-Builder",
       img: "assets/resume-builder.jpg",
       link: "https://github.com/shelbystrawberry/resume-builder",
-      desc: "Resume Builder uses document object model to build an on-the-fly resume",
+      desc: "Resume Builder uses document object model to build a resume on-the-fly",
     },
     {
       id: 3,
       title: "Student-Registrar",
       img: "assets/student-registrar.jpg",
       link: "https://github.com/shelbystrawberry/student-registrar",
-      desc: "Student Registrar aims to encrypt and use python data structures",
+      desc: "Student Registrar uses salt encryption and explores python data structures",
     },
   ];
