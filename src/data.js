2384571948c637528bcb8a67f6c5f27749287d42
shelbystrawberry <shelbsgt500@gmail.com>
2022-05-13 11:55:08 -0400

    data.js for adding creation entries

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const creations = [
+    {
+      id: 1,
+      title: "Personal-Website",
+      img: "assets/portfolio.jpg",
+      link: "https://github.com/shelbystrawberry/personal-website",
+      desc: "Portfolio built with React.js leveraging Material UI",
+    },
+    {
+      id: 2,
+      title: "Resume-Builder",
+      img: "assets/resume-builder.jpg",
+      link: "https://github.com/shelbystrawberry/resume-builder",
+      desc: "Resume Builder uses document object model to build an on-the-fly resume",
+    },
+    {
+      id: 3,
+      title: "Student-Registrar",
+      img: "assets/student-registrar.jpg",
+      link: "https://github.com/shelbystrawberry/student-registrar",
+      desc: "Student Registrar aims to encrypt and use python data structures",
+    },
+  ];
